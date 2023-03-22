0a21ab75fd281698467436c612f52ca140e9b941
Silas Bendix <srb@keycore.dk>
2023-03-22 14:48:07 +0100

    Add skip confirmation

--- bash_scripts/deploy_env.sh
+++ bash_scripts/deploy_env.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 SCRIPT_NAME=$(basename "$0")
-#set -eET -o pipefail 
+#set -eET -o pipefail
 
 # Color variables
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 # Default values
 BUILD_VERSION=""
+CONFIRMATION=false
+USER_UPPERCASE=${USER^^}
+DATE_STR=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
+ENVIRONMENT="dev"
 
 usage() {
-    echo "Usage: ${SCRIPT_NAME} -b <build_version> [-v]" >&2
-    echo "  -b, --build-version <build_version>  Specify the build version to use" >&2
-    echo "  -v, --verbose                        Print verbose output" >&2
-    echo "  -h, --help                           Print this help message" >&2
-    echo "" >&2    
-    echo "Example: ${SCRIPT_NAME} -b 1.2.3 -v" >&2    
+    echo "Usage: ${SCRIPT_NAME} -b <build_version> [-v] [-h] [-y]" >&2
+    echo "  -b <build_version>  Specify the build version to use" >&2
+    echo "  -v                  Print verbose output" >&2
+    echo "  -h                  Print this help message" >&2
+    echo "  -y                  Skip confirmation" >&2
+    echo "" >&2
+    echo "Example: ${SCRIPT_NAME} -b 1.2.3 -v" >&2
 }
 
 # Parse command line arguments
-
-while getopts "b:hv" opt; do
+while getopts "b:hvy" opt; do
     case $opt in
     # -b switch for build version
     b)
@@ -28,25 +34,49 @@ while getopts "b:hv" opt; do
         ;;
     # -v switch for verbose output
     v) set -x ;;
+
+    # -y to skip confirmation
+    y)
+        CONFIRMATION=true
+        ;;
     # -h switch for help
     h)
-        usage        
+        usage
         exit 0
         ;;
     \?)
         echo -e "${RED}Invalid option: -$OPTARG${NC}" >&2
         usage
-        exit 1        
+        exit 1
         ;;
     esac
 done
 
 # Validate build version input
-
 if ! [[ $BUILD_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo -e "${RED}Invalid version number. It must be a valid sematic version, ex. 1.2.3${NC}" >&2
     usage
-    exit 1    
+    exit 1
 fi
 
+
+echo "**************************************************"
+echo "Build version: $BUILD_VERSION"
+echo "Deploying to environment: $ENVIRONMENT"
 echo "BUILD_VERSION: $BUILD_VERSION"
+echo "USER: $USER_UPPERCASE"
+echo "DATE: $DATE_STR"
+echo "**************************************************"
+
+#Prompt for confirmation if -y not specified
+if [[ $CONFIRMATION == false ]]; then
+    read -p "Are you sure you want to deploy version $BUILD_VERSION? (y/n) " -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then 
+        echo -e "${YELLOW}Aborting deployment${NC}"
+        exit 1
+    fi
+fi
+echo -e "${GREEN}Deploying version ${BUILD_VERSION}${NC}"
+
+# do dangerous stuff
